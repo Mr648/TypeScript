d482d5ccac9aabab62561cee7d1bc376774fff47
Rahmat Waisi <rahmatwaisi@gmail.com>
2023-05-29 01:41:53 +0330

    FEAT: add tsconfig

---
+++ Hello.ts
@@ -0,0 +1,8 @@
+const helloTs: string = 'Hello typescript!';
+console.log(helloTs);
+
+console.log('Yo raphael');
+console.log('Yo raphael');
+console.log('Yo raphael');
+console.log('Yo raphael');
+console.log('Yo raphael');
